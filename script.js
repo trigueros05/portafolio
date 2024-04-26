9d851a8230a5bec05347edce5d037cf32e4301fb
trigueros05 <71683493+trigueros05@users.noreply.github.com>
2024-04-26 13:07:16 -0600

    Add files via upload
    Subir la información del Encriptador.

---
+++ script.js
@@ -0,0 +1,19 @@
+const campo_texto= document.querySelector("#texto-encriptado");
+const campo_mensaje=document.querySelector("#campo-mensaje");
+
+const matriz_code = [
+    ["e", "enter"], //indice 0
+    ["i","imes"], //indice 1
+    ["a","ai"], //indice 2
+    ["o","ober"], //indice 3
+    ["u", "ufat"]// indice 4
+]; 
+
+function btnEncriptar {
+    const texto = texto-encriptado.value;
+    console.log(texto);
+}
+
+function btnEncriptar(fraseEncriptada){
+    for(let)
+}
